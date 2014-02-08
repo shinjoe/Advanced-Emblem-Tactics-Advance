407279b765b12beb384d94aa25c91bc3e646326b
sunoya <ejmallare@gmail.com>
2014-02-07 18:18:59 -0800

    Added discover peer thing

--- aeta-android/src/com/cs117/aeta/MenuActivity.java
+++ aeta-android/src/com/cs117/aeta/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.cs117.aeta;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,13 +12,19 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MenuActivity extends Activity {
 	
 	private Button mDiscoverButton;
 	private Button mPlayButton;
+	private ListView mPeerList;
+	
+	private ArrayList peerArrayList;
+	private ArrayAdapter peerAdapter;
 	
 	private final IntentFilter mIntentFilter = new IntentFilter();
 	
@@ -24,6 +33,15 @@ public class MenuActivity extends Activity {
 	private BroadcastReceiver mReceiver;
 	
 	private boolean mWifiP2pEnabled = false;
+	
+	public ArrayAdapter getListAdapter(){
+		return peerAdapter;
+	}
+	
+	public ArrayList getArrayList()
+	{
+		return peerArrayList;
+	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,9 +65,30 @@ public class MenuActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				// discover peers
+				Toast toast = Toast.makeText(getApplicationContext(), "Another test", Toast.LENGTH_LONG);
+				toast.show();
+				mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+
+			        @Override
+			        public void onSuccess() {
+			        	Toast toast = Toast.makeText(getApplicationContext(), "You actually have friends", Toast.LENGTH_LONG);
+			        	toast.show();
+			        }
+
+			        @Override
+			        public void onFailure(int reasonCode) {
+			            Toast toast = Toast.makeText(getApplicationContext(), "No peers", Toast.LENGTH_LONG);
+			            toast.show();
+			        }
+				});
 			}
 		});
+        
+        mPeerList = (ListView) findViewById(R.id.peer_list);
+        peerArrayList = new ArrayList();
+        peerAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, peerArrayList);
+        mPeerList.setAdapter(peerAdapter);
+        //derp.clear();
         
         mPlayButton = (Button) findViewById(R.id.play_button);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
--- aeta-android/src/com/cs117/aeta/WiFiDirectBroadcastReceiver.java
+++ aeta-android/src/com/cs117/aeta/WiFiDirectBroadcastReceiver.java
@@ -1,10 +1,17 @@
 package com.cs117.aeta;
 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -24,6 +31,33 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 	private Channel mChannel;
 	private MenuActivity mActivity;
 	
+	private ArrayList peers;
+
+    private PeerListListener peerListListener = new PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+        	peers = mActivity.getArrayList();
+            // Out with the old, in with the new.
+            peers.clear();
+            peers.addAll(peerList.getDeviceList());
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change.  For instance, if you have a ListView of available
+            // peers, trigger an update.
+            mActivity.getListAdapter().notifyDataSetChanged();
+            if (peers.size() == 0) {
+            	Toast toast = Toast.makeText(mActivity.getApplicationContext(), "yolo", Toast.LENGTH_LONG);
+            	toast.show();
+                return;
+            }
+            else {
+            	Toast toast = Toast.makeText(mActivity.getApplicationContext(), "swag", Toast.LENGTH_LONG);
+            	toast.show();
+            }
+        }
+    };
+
 	public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
 			MenuActivity activity) {
 		this.mManager = manager;
@@ -50,6 +84,16 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 			}
 		} else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 			// Call WifiP2pManager.requestPeers() to get a list of current peers
+			 //Toast toast = Toast.makeText(context, "Something here.", Toast.LENGTH_LONG);
+			 //toast.show();
+			
+			if (mManager != null) {
+				Toast toast= Toast.makeText(context, "yay", Toast.LENGTH_LONG);
+				toast.show();
+	            mManager.requestPeers(mChannel, peerListListener);
+	        }
+
+			 
 		} else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
 			// Respond to new connection or disconnections
 		} else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
