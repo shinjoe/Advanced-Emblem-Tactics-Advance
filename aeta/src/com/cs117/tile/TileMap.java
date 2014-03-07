0840aa2c019e2f3757ca8709aa835a05eb0d33ab
sunoya <ejmallare@gmail.com>
2014-03-06 21:26:04 -0800

    Fixed attacking same team

--- aeta/src/com/cs117/tile/TileMap.java
+++ aeta/src/com/cs117/tile/TileMap.java
@@ -263,6 +263,9 @@ public class TileMap {
 					Coordinate currUnit = new Coordinate(prevX, prevY);
 					Unit attacking = unitMap.get(currUnit);
 					
+					if(attacking.getTeam() == attacked.getTeam())
+						break;
+					
 					attacked.getAttacked(attacking);
 					if(attacked.getHp() <= 0)
 					{
